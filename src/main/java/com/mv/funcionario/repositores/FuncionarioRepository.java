8f33495d22366302df6ca383f887d420e9ac5213
aranavitoria <128552019+MariaVitoriaArana@users.noreply.github.com>
2026-09-17 12:38:01 -0300

    corrigido e arrumado

--- src/main/java/com/mv/funcionario/repositores/FuncionarioRepository.java
+++ src/main/java/com/mv/funcionario/repositores/FuncionarioRepository.java
@@ -1,4 +1,4 @@
-package com.mv.funcionario.repositores;
+package com.mv.funcionario.repositories;
 
 import com.mv.funcionario.models.FuncionarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
