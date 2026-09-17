4369fbedb713971e2b70ec74f74c07885c8eb100
aranavitoria <128552019+MariaVitoriaArana@users.noreply.github.com>
2026-09-17 12:35:04 -0300

    Arrumado

--- src/main/java/com/mv/funcionario/controllers/FuncionarioController.java
+++ src/main/java/com/mv/funcionario/controllers/FuncionarioController.java
@@ -1,6 +1,5 @@
 package com.mv.funcionario.controllers;
 
-
 import com.mv.funcionario.models.FuncionarioModel;
 import com.mv.funcionario.services.FuncionarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,37 +8,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/funcinarios")
-
+@RequestMapping(path = "/funcionarios")
 public class FuncionarioController {
 
     @Autowired
     private FuncionarioServices funcionarioServices;
 
+    // BUSCAR TODOS
     @GetMapping
-    public List<FuncionarioModel> findAll(){
+    public List<FuncionarioModel> findAll() {
         return funcionarioServices.findAll();
     }
 
+    // BUSCAR POR ID
     @GetMapping("/{id}")
-    public FuncionarioModel buscarPorId(@PathVariable Long id){
+    public FuncionarioModel buscarPorId(@PathVariable Long id) {
         return funcionarioServices.buscarPorId(id);
     }
 
+    // DELETAR
     @DeleteMapping("/{id}")
-    public void deletar(Long id){
+    public void deletar(@PathVariable Long id) {
         funcionarioServices.deletar(id);
     }
 
+    // CRIAR
     @PostMapping
-    public FuncionarioModel criarFuncionario(@RequestBody FuncionarioModel funcionarioModel){
-        return  funcionarioServices.criarPessoa(funcionarioModel);
+    public FuncionarioModel criarFuncionario(@RequestBody FuncionarioModel funcionarioModel) {
+        return funcionarioServices.criarFuncionario(funcionarioModel);
     }
 
+    // ATUALIZAR
     @PutMapping("/{id}")
-    public FuncionarioModel atualizar(@PathVariable Long id,@RequestBody FuncionarioModel funcionarioModel){
-        return funcionarioServices.atualizar(id,funcionarioModel);
+    public FuncionarioModel atualizar(
+            @PathVariable Long id,
+            @RequestBody FuncionarioModel funcionarioModel) {
+
+        return funcionarioServices.atualizar(id, funcionarioModel);
     }
-
-
 }
+```
