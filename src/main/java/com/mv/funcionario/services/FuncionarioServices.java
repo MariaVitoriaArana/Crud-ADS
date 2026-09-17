17c8f6e744f84f80c5382d53902bc84874b742f5
aranavitoria <128552019+MariaVitoriaArana@users.noreply.github.com>
2026-09-17 12:36:58 -0300

    corrigido e arrumado

--- src/main/java/com/mv/funcionario/services/FuncionarioServices.java
+++ src/main/java/com/mv/funcionario/services/FuncionarioServices.java
@@ -5,7 +5,6 @@ import com.mv.funcionario.repositores.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.List;
 
 @Service
@@ -14,31 +13,34 @@ public class FuncionarioServices {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
-    public List<FuncionarioModel> findAll(){
+    // BUSCAR TODOS
+    public List<FuncionarioModel> findAll() {
         return funcionarioRepository.findAll();
     }
 
-    public FuncionarioModel buscarPorId (Long id){
+    // BUSCAR POR ID
+    public FuncionarioModel buscarPorId(Long id) {
         return funcionarioRepository.findById(id).get();
     }
 
-    public FuncionarioModel criarPessoa(FuncionarioModel funcionarioModel){
+    // CRIAR
+    public FuncionarioModel criarFuncionario(FuncionarioModel funcionarioModel) {
         return funcionarioRepository.save(funcionarioModel);
     }
 
-    //deletar
-
-    public void deletar(Long id){
+    // DELETAR
+    public void deletar(Long id) {
         funcionarioRepository.deleteById(id);
     }
 
-    //atualizar
-    public FuncionarioModel atualizar(Long id, FuncionarioModel funcionarioModel){
+    // ATUALIZAR
+    public FuncionarioModel atualizar(Long id, FuncionarioModel funcionarioModel) {
+
         FuncionarioModel func = funcionarioRepository.findById(id).get();
+
         func.setNome(funcionarioModel.getNome());
         func.setCpf(funcionarioModel.getCpf());
+
         return funcionarioRepository.save(func);
     }
-
-
 }
